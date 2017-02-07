787680f1b8ab53bc9b3f556d75b1e7e4dbf97000
Sylla <syllamohamedlamine17@gmail.com>
2017-02-07 10:07:14 +0000

    modèle modifié

--- model.js
+++ model.js
@@ -1,3 +1,53 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing() {
+  this.lsForms = new Array();
+  
+  this.getForme = fonction(){
+	  
+	  return this.lsForms;
+  },
+  
+  this.addForm = fonction(lsForm){
+	  
+	  this.lsForm.push(lsForm) ;
+	  
+  }
+};
+
+function Forme(epaisseur, couleur) {
+  this.epaisseur = epaisseur;
+  this.couleur = couleur;
+  
+  this.getEpaisseur = fonction(){
+	  
+	  return this.epaisseur;
+  },
+  
+  this.getCouleur = fonction(){
+	  
+	  return this.couleur;
+	  
+  }
+};
+
+function Rectangle(x, y, largeur, hauteur, epaisseur, couleur) {
+  Forme.call(this, epaisseur, couleur);
+  this.x = x;
+  this.y = y;
+  this.largeur = largeur;
+  this.hauteur = hauteur;
+  
+ 	  
+  }
+  
+};
+Rectangle.prototype = new Forme();
+
+function Ligne(x, y, epaisseur, couleur) {
+  Forme.call(this, epaisseur, couleur);
+  this.x = x;
+  this.y = y;
+};
+Ligne.prototype = new Forme();
